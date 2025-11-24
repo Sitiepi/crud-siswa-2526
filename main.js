977f142ba11b90438dec9b14fe9d5a58fc6f0a99
Sitiepi <sitiepi7@gmail.com>
2025-11-24 10:38:28 +0700

    update

--- main.js
+++ main.js
@@ -13,17 +13,17 @@ import {
 
 // GANTI DENGAN FIREBASE CONFIG ANDA
 const firebaseConfig = {
-    apiKey: "AIzaSyDgGanI0xfwbMbF2Q20eftio7Hc6iyPVgI",
-    authDomain: "insancemerlang-e9c87.firebaseapp.com",
-    projectId: "insancemerlang-e9c87",
-    storageBucket: "insancemerlang-e9c87.firebasestorage.app",
-    messagingSenderId: "1009245252263",
-    appId: "1:1009245252263:web:637bfe528eddfc0dc18982"
+  apiKey: "AIzaSyDgGanI0xfwbMbF2Q20eftio7Hc6iyPVgI",
+  authDomain: "insancemerlang-e9c87.firebaseapp.com",
+  projectId: "insancemerlang-e9c87",
+  storageBucket: "insancemerlang-e9c87.firebasestorage.app",
+  messagingSenderId: "1009245252263",
+  appId: "1:1009245252263:web:637bfe528eddfc0dc18982"
 };
 
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app)
-const siswaCollection = collection(db,"siswa")
+const siswaCollection = collection(db, "siswa")
 
 // fungsi untuk menampilkan daftar siswa
 export async function tampilkanDaftarSiswa() {
@@ -84,4 +84,23 @@ export async function tampilkanDaftarSiswa() {
     //tambahkan baris kedalam tabel
     tabel.appendChild(baris)
   })
+}
+
+
+//fungsi untuk menambahkan data siswa
+export async function tambahDataSiswa() {
+  //ambil nilai dari form
+  const nis = document.getElementById('nis').value
+  const nama = document.getElementById('nama').value
+  const kelas = document.getElementById('kelas').value
+  
+  //tambahkan data ke firestore
+  await addDoc(siswaCollection, {
+    nis: nis,
+    nama: nama,
+    kelas: kelas
+  })
+  
+  //alihkan ke halaman daftar siswa
+  window.location.href = 'daftar.html'
 }
